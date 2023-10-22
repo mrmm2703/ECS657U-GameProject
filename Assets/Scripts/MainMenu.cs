f034c2611d357c4aa34bc847bce3786064d183db
TAJULAHMED <98593713+TAJULAHMED@users.noreply.github.com>
2023-10-22 19:41:57 +0100

    Main Menu Screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //or 
+        //SceneManager.LoadScene("Scene Name
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has quit the game");
+    }
+}
