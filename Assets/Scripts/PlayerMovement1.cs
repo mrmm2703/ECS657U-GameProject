ca4a753a275df57474bed1ea7c9c03570c1b575e
mRahmanCodes <mayezrahman1@gmail.com>
2024-01-07 19:28:10 +0000

    Redone camera movement

---
+++ Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public BoxCollider cameraBounds;
+    private float speed = 25f;
+
+    public Transform orientation;
+
+    float horizontalInp;
+    float verticalInp;
+
+    Vector3 moveDir;
+
+    Rigidbody rb;
+
+    private void OnTriggerExit(Collider other)
+    {
+        MovePlayer(-1, ForceMode.Impulse);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(1, ForceMode.Force);
+    }
+    private void MyInput()
+    {
+        horizontalInp = Input.GetAxisRaw("Horizontal");
+        verticalInp = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer(int oppositeOrNot, ForceMode forcetype)
+    {
+        moveDir = (orientation.forward * verticalInp + orientation.right * horizontalInp) * oppositeOrNot;
+
+        rb.AddForce(moveDir.normalized * speed * 10f, forcetype);
+
+    }
+}
